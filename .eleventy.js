62c4628e162745eb1d8926595b9f88d1a1f71d19
oss6 <ossedb@gmail.com>
2020-11-03 22:09:49 +0000

    Update website

--- .eleventy.js
+++ .eleventy.js
@@ -12,7 +12,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy("images");
   eleventyConfig.addPassthroughCopy({ "_build/css": "css" });
-  eleventyConfig.addPassthroughCopy("index.js");
   eleventyConfig.addPassthroughCopy("favicon.ico");
   eleventyConfig.addPassthroughCopy("favicon-16x16.png");
   eleventyConfig.addPassthroughCopy("favicon-32x32.png");
